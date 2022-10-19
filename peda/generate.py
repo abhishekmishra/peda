d67585d3d922afe93af5c15024da90f4c5171c81
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-10-19 11:45:46 +0530

    generating main.cpp file with project file

--- peda/generate.py
+++ peda/generate.py
@@ -6,16 +6,30 @@ app = typer.Typer()
 
 
 @app.command()
-def cmakeMinimal(projectName: str = "hello-world", mainFile: str = "main.cpp"):
+def cmakeMinimal(projectName: str = "hello-world", mainFile: str = "main.cpp", lang: str = "CXX"):
     print(f"Creating {projectName} with file {mainFile}")
     os.mkdir(projectName)
     with open(os.path.join(projectName, 'CMakeLists.txt'), 'w') as cf:
         cf.write(textwrap.dedent(f"""
         cmake_minimum_required(VERSION 3.22)
 
-        project({projectName})
+        project({projectName} {lang})
 
         add_executable({projectName}
             {mainFile}
         )
         """))
+    
+    if mainFile.lower().endswith('.cpp'):
+        with open(os.path.join(projectName, mainFile), 'w') as pf:
+            pf.write(textwrap.dedent(f"""
+            #include <iostream>
+
+            using namespace std;
+
+            int main(int argc, char* argv[]) 
+            {{
+                cout << "hello world" << endl;
+                return 0;
+            }}
+            """))
