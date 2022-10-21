bf39c19bf4f539d620339b03154e755fab9d259f
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-10-21 09:37:47 +0530

    prettify run command output

--- peda/utils.py
+++ peda/utils.py
@@ -1,5 +1,10 @@
 import subprocess
+from rich import print
 
 def runCommand(cmdArr):
-    print(cmdArr)
-    return subprocess.run(cmdArr)
+    print("[bold green underline]========================================================[/bold green underline]")
+    print(f"[bold green underline]PEDA: :person_running: command: [/bold green underline]{cmdArr}")
+    print("[bold green underline]========================OUTPUT==========================[/bold green underline]")
+    res = subprocess.run(cmdArr)
+    print("[bold green underline]=========================DONE===========================[/bold green underline]")
+    return res
