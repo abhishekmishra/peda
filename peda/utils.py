da7d404b99fd224d4e93d51e5e872896d5d515e3
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-10-25 11:15:59 +0530

    removed underline for some prints

--- peda/utils.py
+++ peda/utils.py
@@ -2,9 +2,9 @@ import subprocess
 from rich import print
 
 def runCommand(cmdArr):
-    print("[bold green underline]========================================================[/bold green underline]")
+    print("[bold green]========================================================[/bold green]")
     print(f"[bold green underline]PEDA: :person_running: command: [/bold green underline]{cmdArr}")
-    print("[bold green underline]========================OUTPUT==========================[/bold green underline]")
+    print("[bold green]========================OUTPUT==========================[/bold green]")
     res = subprocess.run(cmdArr)
-    print("[bold green underline]=========================DONE===========================[/bold green underline]")
+    print("[bold green]=========================DONE===========================[/bold green]")
     return res
